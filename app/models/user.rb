934fbf909ba25cd765153f8a77bafff98da2c57b
Tony Coconate <me@tonycoconate.com>
2010-08-27 16:37:26 -0500

    The model was wrong... I should test more.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ActiveRecord::Base
 
     my_rank = Ranking.where(:tournament_id => ranking.tournament.id, :user_id => self.id).first
 
-    return ((ranking.rank - my_rank.rank) < 3) && !(ranking.rank > my_rank.rank) ? true : false
+    return ((my_rank.rank - ranking.rank) <= 3) && !(ranking.rank > my_rank.rank) ? true : false
   end
 end
